d0f218b69c082d488242aee2e29cc78df5505ffa
iKenshu <undefined.hlo.o@gmail.com>
2017-10-03 11:09:22 -0400

    methods for objects js

--- dos_puntos.js
+++ dos_puntos.js
@@ -1,20 +1,24 @@
 // Calcular la distancia entre dos puntos
 
 const punto1 = {
-    X: 0,
-    Y: 4
+    x: 0,
+    y: 4,
+    moverEnX: function (x) { this.x += x },
+    moverEnY: function (y) { this.y += y }
 }
 
 const punto2 = {
-    X: 3, 
-    Y: 0
+    x: 3, 
+    y: 0
+    moverEnX: function (x) { this.x += x },
+    moverEnY: function (y) { this.y += y }
 }
 
 function distancia(punto1, punto2) {
-    const x = punto1.X - punto2.X
-    const y = punto1.Y - punto2.Y
+    const x = punto1.x - punto2.x
+    const y = punto1.y - punto2.y
 
-    return Math.sqrt(x * x + y * y)
+    return Math.sqrt(x * x + y * y).toFixed(2)
 }
 
 console.log(distancia(punto1, punto2))
