99037e00545ca2e5f88170f6bfbdc4b9fc3eb4e0
iKenshu <undefined.hlo.o@gmail.com>
2017-10-03 12:34:26 -0400

    Scope variables

---
+++ scope.js
@@ -0,0 +1,11 @@
+function saludarAKevin10(nombre) {
+    const = 'Kevin'
+    
+    for (let i = 0; i < 10; i++) {
+        console.log(`hola ${nombre}`)
+    }
+
+    console.log(`El valor de i es: ${i}`)
+}
+
+saludarAKevin10()
