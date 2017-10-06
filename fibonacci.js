82f59e2ad6626463cd28219b3a51049556ba20a2
iKenshu <undefined.hlo.o@gmail.com>
2017-10-06 16:43:34 -0400

    memoizacion

--- fibonacci.js
+++ fibonacci.js
@@ -1,6 +1,20 @@
-function fibonacci (num) {
+let contadorMemo = 1
+
+function fibonacciMemo(num, memoria = {}) {
+    contadorMemo++
+    if (memoria[num]) return memoria[num]
     if (num == 1) return 0
     if (num == 2) return 1
 
-    return fibonacci(num - 1) + fibonacci(num - 2)
+    return memoria[num] = fibonacciMemo(num - 1, memoria) + fibonacciMemo(num - 2, memoria)
+}
+
+let contadorRec = 1
+
+function fibonacciRecursivo(num) {
+    contadorRec++
+    if (num == 1) return 0
+    if (num == 2) return 1
+
+    return fibonacciRecursivo(num - 1) + fibonacciRecursivo(num - 2)
 }
